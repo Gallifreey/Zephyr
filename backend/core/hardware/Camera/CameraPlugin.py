094d37af140db9f3f94fc0e537346494ba4f74bf
Gallifreey <3291856866@qq.com>
2024-02-25 13:30:17 +0800

    Remove 'Camera' From 'Diver'.

---
+++ backend/core/hardware/Camera/CameraPlugin.py
@@ -0,0 +1,23 @@
+from core.hardware.Camera.USBCamera import USBCamera
+from core.library.PluginBase import PluginBase
+from core.utils.logger import Logger
+
+
+class CameraPlugin(PluginBase):
+    __camera = USBCamera()
+    camera_view = None  # 摄像头视野
+    plugin_name = 'camera'
+
+    def __init__(self):
+        super().__init__()
+        self.priority = 1
+
+    @classmethod
+    def start(cls):
+        cls.__camera.parent = cls
+        cls.__camera.open()
+        Logger.info(f'{cls.__camera.name} open.')
+
+    @classmethod
+    def uninstall(cls):
+        cls.__camera.close()
